763e27be5a98e535b8cf802c6d116e1df2c9054b
TuVoXuan <voxuantu1235@gmail.com>
2025-08-04 10:43:12 +0700

    feat: update responsive mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -29,23 +29,28 @@ export default function Header() {
     const navMenu = document.getElementById("navMenu");
     if (navMenu) {
       navMenu.classList.toggle("-translate-y-[calc(100%+55px)]");
+      navMenu.classList.toggle("md:-translate-y-[calc(100%+76px)]");
     }
   };
 
   return (
     <header className="absolute top-0 inset-x-0 z-50">
       <div className="relative">
-        <div className="relative h-[55px] bg-black/60 pl-[76px] pr-[10px] flex justify-between items-center z-10">
+        <div className="relative h-[55px] md:h-[76px] bg-black/60 pl-[76px] md:pl-[104.5px] pr-[10px] md:pr-[13.75px] flex justify-between items-center z-10">
           <Image
             src={logo.src}
             height={logo.height}
             width={logo.width}
-            className="absolute top-[5px] left-[10px] h-[59px] w-[59px] z-10"
+            className="absolute top-[5px] md:top-[6.875px] left-[10px] md:left-[13.75px] h-[59px] w-[59px] md:h-[81.25px] md:w-[81.25px] z-10"
             alt="Soul Knight Prequel Logo"
           />
           <div>
-            <h1 className="text-[16px] text-white">Soul Knight Prequel</h1>
-            <h2 className="text-[10px] text-white/60">Diablo-like ARPG</h2>
+            <h1 className="text-[16px] md:text-[22px] text-white">
+              Soul Knight Prequel
+            </h1>
+            <h2 className="text-[10px] md:text-[13.75px] text-white/60">
+              Diablo-like ARPG
+            </h2>
           </div>
 
           <button>
@@ -53,36 +58,36 @@ export default function Header() {
               src={downloadButton.src}
               height={downloadButton.height}
               width={downloadButton.width}
-              className="h-[28px] w-[100px]"
+              className="h-[28px] w-[100px] md:w-[137.5px] md:h-[38.5px]"
               alt="Download Button"
             />
           </button>
 
           <ul onClick={handleClickMenuIcon}>
-            <li className="w-5 h-[2px] bg-[#00a2e3] rounded-[2px] my-[5px]"></li>
-            <li className="w-5 h-[2px] bg-[#00a2e3] rounded-[2px] my-[5px]"></li>
-            <li className="w-5 h-[2px] bg-[#00a2e3] rounded-[2px] my-[5px]"></li>
+            <li className="w-5 md:w-[27.5px] h-[2px] md:h-[2.75px] bg-[#00a2e3] rounded-[2px] md:rounded-[2.75px] my-[5px] md:my-[6.875px]"></li>
+            <li className="w-5 md:w-[27.5px] h-[2px] md:h-[2.75px] bg-[#00a2e3] rounded-[2px] md:rounded-[2.75px] my-[5px] md:my-[6.875px]"></li>
+            <li className="w-5 md:w-[27.5px] h-[2px] md:h-[2.75px] bg-[#00a2e3] rounded-[2px] md:rounded-[2.75px] my-[5px] md:my-[6.875px]"></li>
           </ul>
         </div>
         <div
           id="navMenu"
-          className="bg-nav-menu-mobile absolute top-full -translate-y-[calc(100%+55px)] left-0 w-screen px-[90px] pt-[10px] pb-[20px] transition-transform duration-500 ease-in-out"
+          className="bg-nav-menu-mobile absolute top-full -translate-y-[calc(100%+55px)] md:-translate-y-[calc(100%+76px)] left-0 w-screen px-[90px] md:px-[123.75px] pt-[10px] md:pt-[13.75px] pb-[20px] md:pb-[27.5px] transition-transform duration-500 ease-in-out"
         >
           <ul>
             {NavMenuItems.map((item) => (
               <li
                 key={item.href}
-                className="py-[10px] text-[#757575] text-[18px] font-medium border-b-[1px] border-b-solid border-b-[#cecece]/60 flex justify-center items-center"
+                className="py-[10px] md:py-[13.75px] text-[#757575] text-[18px] md:text-[24.75px] font-medium border-b-[1px] border-b-solid border-b-[#cecece]/60 flex justify-center items-center"
               >
                 {item.label}
               </li>
             ))}
-            <li className="py-[10px] flex items-center flex-wrap border-b-[1px] border-b-solid border-b-[#cecece]/60">
+            <li className="py-[10px] md:py-[13.75px] flex items-center flex-wrap border-b-[1px] border-b-solid border-b-[#cecece]/60">
               {SocialMediaIcons.map((item) => (
-                <span key={item.alt} className="px-[10px]">
+                <span key={item.alt} className="px-[10px] md:px-[13.75px]">
                   {
                     <Image
-                      className="w-[40px] h-[30px] shrink-0 my-[5px] mx-[2px]"
+                      className="w-[40px] md:w-[55px] h-[30px] md:h-[41.25px] shrink-0 my-[5px] md:my-[6.875px] mx-[2px] md:mx-[2.75px]"
                       src={item.src}
                       height={80}
                       width={80}
@@ -92,7 +97,7 @@ export default function Header() {
                 </span>
               ))}
             </li>
-            <li className="pt-[15px] flex justify-center">
+            <li className="pt-[15px] md:pt-[20.625px] flex justify-center">
               <button>
                 <Image
                   className="w-[20px] h-auto"
--- src/components/Overview/index.tsx
+++ src/components/Overview/index.tsx
@@ -28,13 +28,13 @@ export default function Overview() {
   }, []);
 
   return (
-    <main className="relative h-full pt-[95px] overview-section flex flex-col justify-center items-center">
+    <main className="relative h-full pt-[95px] md:pt-[130.625px] overview-section flex flex-col justify-center items-center">
       <div>
         <Image
           src={overviewTitle.src}
           height={overviewTitle.height}
           width={overviewTitle.width}
-          className="w-[283px] h-[168px]"
+          className="w-[283px] h-[168px] md:w-[389.125px] md:h-[231px]"
           alt="Overview Title"
         />
       </div>
@@ -43,47 +43,47 @@ export default function Overview() {
           src={playVideoBtn.src}
           height={playVideoBtn.height}
           width={playVideoBtn.width}
-          className="w-[78px] h-[78px] cursor-pointer"
+          className="w-[78px] h-[78px] md:w-[107.25px] md:h-[107.25px] cursor-pointer"
           alt="Play Video Button"
         />
       </div>
-      <div className="h-[450px] w-[912px] relative z-10 overflow-hidden">
-        <span className="absolute earth-bg-animation background-earth block top-10 h-[912px] w-full"></span>
+      <div className="h-[450px] md:h-[618.75px] w-[912px] md:w-[1254px] relative z-10 overflow-hidden">
+        <span className="absolute earth-bg-animation background-earth block top-10 md:top-[55px] h-[912px] md:h-[1254px] w-full"></span>
         <div>
           <Image
             src={NPCCharacter.src}
             height={NPCCharacter.height}
             width={NPCCharacter.width}
-            className="absolute top-[60px] left-1/2 -translate-x-[90px] w-[200px] h-auto z-30"
+            className="absolute top-[60px] md:top-[82.5px] left-1/2 -translate-x-[90px] md:-translate-x-[123.75px] w-[200px] md:w-[275px] h-auto z-30"
             alt="NPC Character"
           />
           <Image
             src={NPCShadow.src}
             height={NPCShadow.height}
             width={NPCShadow.width}
-            className="absolute top-[200px] left-1/2 -translate-x-[45px] w-[90px] h-auto z-20"
+            className="absolute top-[200px] md:top-[275px] left-1/2 -translate-x-[45px] md:-translate-x-[61.875px] w-[90px] md:w-[123.75px] h-auto z-20"
             alt="NPC Shadow"
           />
-          <div className="z-20 absolute top-[55px] left-1/2 w-[55px] h-[50px] translate-x-[36px]">
+          <div className="z-20 absolute top-[55px] md:top-[75.625px] left-1/2 w-[50px] md:w-[68.75px] h-[50px] md:h-[68.75px] translate-x-[36px] md:translate-x-[49.5px]">
             <span className={talkIconClass}></span>
           </div>
-          <div className="absolute top-[250px] flex z-30 justify-center w-full">
-            <div className="w-[156px] cursor-pointer">
+          <div className="absolute top-[250px] md:top-[343.75px] flex z-30 justify-center w-full">
+            <div className="w-[156px] md:w-[214.5px] cursor-pointer">
               <Image
                 src={downloadBtn.src}
                 height={downloadBtn.height}
                 width={downloadBtn.width}
-                className="w-[156px] h-[57px] cursor-pointer"
+                className="w-[156px] md:w-[214.5px] h-[57px] md:h-[78.375px] cursor-pointer"
                 alt="Download Button"
               />
             </div>
           </div>
         </div>
       </div>
-      <span className="cloud-top-left-1 absolute top-[70px] w-[175px] h-[100px] left-[-120px]"></span>
-      <span className="cloud-top-left-2 absolute w-[78px] h-[54px] top-[130px] -right-8"></span>
-      <span className="cloud-middle-left-1 absolute w-[114px] h-[64px] top-[300px] left-[18px]"></span>
-      <span className="cloud-right-1 absolute w-[218px] h-[116px] top-[235px] right-[-120px]"></span>
+      <span className="cloud-top-left-1 absolute top-[70px] md:top-[96.25px] w-[175px] md:w-[240.625px] h-[100px] md:h-[137.5px] left-[-120px] md:left-[-165px]"></span>
+      <span className="cloud-top-left-2 absolute w-[78px] md:w-[107.25px] h-[54px] md:h-[74.25px] top-[130px] md:top-[178.75px] -right-8 md:-right-[44px]"></span>
+      <span className="cloud-middle-left-1 absolute w-[114px] md:w-[156.75px] h-[64px] md:h-[88px] top-[300px] md:top-[412.5px] left-[18px] md:left-[24.75px]"></span>
+      <span className="cloud-right-1 absolute w-[218px] md:w-[299.75px] h-[116px] md:h-[159.5px] top-[235px] md:top-[323.125px] right-[-120px] md:right-[-165px]"></span>
     </main>
   );
 }
